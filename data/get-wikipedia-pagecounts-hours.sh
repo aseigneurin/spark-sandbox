355ad9413383338b5a580ef4145a03ad06eee2e8
Alexis Seigneurin <alexis@seigneurin.com>
2014-11-07 10:29:37 +0100

    - code for wikipedia mapreduce

---
+++ data/get-wikipedia-pagecounts-hours.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+if [ ! -d wikipedia-pagecounts ]; then
+  mkdir wikipedia-pagecounts
+fi
+cd wikipedia-pagecounts
+
+yyyy=2014
+MM=11
+dd=01
+
+for hh in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+do
+  wget https://dumps.wikimedia.org/other/pagecounts-raw/${yyyy}/${yyyy}-${MM}/pagecounts-${yyyy}${MM}${dd}-${hh}0000.gz
+  gunzip pagecounts-${yyyy}${MM}${dd}-${hh}0000.gz
+done
---
+++ src/main/java/com/seigneurin/spark/WikipediaMapReduceByKey.java
@@ -0,0 +1,61 @@
+package com.seigneurin.spark;
+
+import java.io.Serializable;
+import java.util.Comparator;
+
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import scala.Tuple2;
+
+public class WikipediaMapReduceByKey {
+
+    public static void main(String[] args) {
+        SparkConf conf = new SparkConf()
+                .setAppName("wikipedia-mapreduce-by-key")
+                .setMaster("local[16]");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaPairRDD<String, Long> rdd = sc.textFile("data/wikipedia-pagecounts-hours/pagecounts-20141101-*")
+                .map(line -> line.split(" "))
+                .mapToPair(s -> new Tuple2<String, Long>(s[0], Long.parseLong(s[2])))
+                .reduceByKey((x, y) -> x + y)
+                .cache();
+
+        // affichage simple
+        rdd
+                .foreach(t -> System.out.println(t._1 + " -> " + t._2));
+
+        // tri
+        rdd
+                .sortByKey()
+                .foreach(t -> System.out.println(t._1 + " -> " + t._2));
+
+        // tri lower-case
+        // génère une exception :
+        //      Task not serializable: java.io.NotSerializableException
+        //        rdd
+        //                .sortByKey(Comparator.comparing(String::toLowerCase))
+        //                .foreach(t -> System.out.println(t._1 + " -> " + t._2));
+
+        // tri lower-case
+        rdd
+                .sortByKey(new LowerCaseStringComparator())
+                .foreach(t -> System.out.println(t._1 + " -> " + t._2));
+
+        // tri par valeurs
+        rdd
+                .mapToPair(t -> new Tuple2<Long, String>(t._2, t._1))
+                .sortByKey(false)
+                .take(10)
+                .forEach(t -> System.out.println(t._2 + " -> " + t._1));
+    }
+
+    private static class LowerCaseStringComparator implements Comparator<String>, Serializable {
+        @Override
+        public int compare(String s1, String s2) {
+            return s1.toLowerCase().compareTo(s2.toLowerCase());
+        }
+    }
+}
