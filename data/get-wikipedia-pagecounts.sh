7f77755c205d0e840a97dca8d0fb93860035705e
Alexis Seigneurin <alexis@seigneurin.com>
2014-11-11 11:01:54 +0100

    - fixed wikipedia download script

--- data/get-wikipedia-pagecounts.sh
+++ data/get-wikipedia-pagecounts.sh
@@ -11,6 +11,5 @@ dd=01
 
 for hh in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23
 do
-  wget https://dumps.wikimedia.org/other/pagecounts-raw/${yyyy}/${yyyy}-${MM}/pagecounts-${yyyy}${MM}${dd}-${hh}0000.gz
-  gunzip pagecounts-${yyyy}${MM}${dd}-${hh}0000.gz
+  curl https://dumps.wikimedia.org/other/pagecounts-raw/${yyyy}/${yyyy}-${MM}/pagecounts-${yyyy}${MM}${dd}-${hh}0000.gz | gunzip - > pagecounts-${yyyy}${MM}${dd}-${hh}0000
 done
